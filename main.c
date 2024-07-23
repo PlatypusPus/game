cf06b776b18b75628b9101fb0215e7ac7e66c3c2
nearlynithin <nearlynithin@gmail.com>
2024-07-24 01:21:07 +0530

    🔧 chore: moving on rotation

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include <raymath.h>
 #include <stdlib.h>
 #include <time.h>
-#define RLIGHTS_IMPLEMENTATION
 #include "rlights.h"
 
 #define GLSL_VERSION 330
@@ -63,43 +62,21 @@ int main(void)
     const int screenWidth = 880;
     const int screenHeight = 450;
 
-    SetConfigFlags(FLAG_MSAA_4X_HINT); // Enable Multi Sampling Anti Aliasing 4x (if available)
+    SetConfigFlags(FLAG_MSAA_4X_HINT); 
     InitWindow(screenWidth, screenHeight, "raylib");
 
     Camera camera = {0};
-    camera.position = (Vector3){0.0f, 4.0f, -100.0f};
-    camera.target = (Vector3){0.0f, 3.0f, 0.0f};
+    camera.position = (Vector3){-5.0f, 0.4f, 2.0f};
+    camera.target = (Vector3){0.185f, 0.4f, -1.0f};
     camera.up = (Vector3){0.0f, 1.0f, 0.0f};
     camera.projection = CAMERA_PERSPECTIVE;
-    camera.fovy = 5.0f;
-    // CameraYaw(&camera, -135 * DEG2RAD, true);
-    // CameraPitch(&camera, -45 * DEG2RAD, true, true, false);
+    camera.fovy = 45.0f;
+
 
     int cameraMode = CAMERA_THIRD_PERSON;
 
     // The Model
     Model model = LoadModel("./grass.obj");
-
-    // Loading the shader and setting up uniforms
-    Shader shader = LoadShader(TextFormat("shaders/glsl%i/lighting.vs", GLSL_VERSION), TextFormat("shaders/glsl%i/fog.fs", GLSL_VERSION));
-    shader.locs[SHADER_LOC_MATRIX_MODEL] = GetShaderLocation(shader, "matModel");
-    shader.locs[SHADER_LOC_VECTOR_VIEW] = GetShaderLocation(shader, "viewPos");
-
-    // Ambient light level
-    int ambienLoc = GetShaderLocation(shader, "ambient");
-    SetShaderValue(shader, ambienLoc, (float[4]){0.3f, 0.3f, 0.3f, 1.0f}, SHADER_UNIFORM_VEC4);
-
-    float fogDensity = 0.01f;
-    int fogDensityLoc = GetShaderLocation(shader, "fogDensity");
-    SetShaderValue(shader, fogDensityLoc, &fogDensity, SHADER_UNIFORM_FLOAT);
-
-    Vector3 fogColor = {1.0f, 1.0f, 1.0f}; // Light gray-blue fog color
-    int fogColorLoc = GetShaderLocation(shader, "fogColor");
-    SetShaderValue(shader, fogColorLoc, &fogColor, SHADER_UNIFORM_VEC3);
-    model.materials[0].shader = shader;
-
-    // Creating light
-    CreateLight(LIGHT_POINT, (Vector3){0, 5, 6}, Vector3Zero(), WHITE, shader);
 
     DisableCursor();
     SetTargetFPS(60);
@@ -109,16 +86,12 @@ int main(void)
     {
         UpdateCamera(&camera, cameraMode);
 
-        SetShaderValue(shader, fogDensityLoc, &fogDensity, SHADER_UNIFORM_FLOAT);
-
-        // Updating the ligth shader with the current camera position
-        SetShaderValue(shader, shader.locs[SHADER_LOC_VECTOR_VIEW], &camera.position.x, SHADER_UNIFORM_VEC3);
 
         BeginDrawing();
         ClearBackground(GRAY);
 
         BeginMode3D(camera);
-        DrawPlane((Vector3){0.0f, 0.0f, 0.0f}, (Vector2){32.0f, 32.0f}, GRAY);
+        DrawPlane((Vector3){0.0f, 0.0f, 0.0f}, (Vector2){32.0f, 32.0f}, LIME);
         DrawGrass(model);
         if (cameraMode == CAMERA_THIRD_PERSON)
         {
@@ -130,7 +103,6 @@ int main(void)
         EndDrawing();
     }
     UnloadModel(model);
-    UnloadShader(shader);
     CloseWindow();
     return 0;
 }
