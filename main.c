4da22171704153266b9232acf086c77e880ea686
nearlynithin <nearlynithin@gmail.com>
2024-07-24 12:01:26 +0530

    🐛 fix: Grass blades sway over time along a sine function. But the DrawModelEx function doesn't support rotation along Y axis.

--- main.c
+++ main.c
@@ -7,8 +7,10 @@
 
 #define GLSL_VERSION 330
 
-#define NUM_GRASS_BLADES 12000
+#define NUM_GRASS_BLADES 15000
 #define FIELD_SIZE 34.0f
+#define PI 3.14159274101257324219f
+#define DEG2RAD (PI / 180.0f)
 
 typedef struct
 {
@@ -49,13 +51,22 @@ void InitGrass()
     }
 }
 
-void DrawGrass(Model model)
+void DrawGrassNew(Model model, float bendfactor )
 {
     for (int i = 0; i < NUM_GRASS_BLADES; i++)
     {
-        DrawModelEx(model, grassBlades[i].position, (Vector3){0.0f, 1.0f, 0.0f}, grassBlades[i].rotation, (Vector3){grassBlades[i].scale, grassBlades[i].scale, grassBlades[i].scale}, DARKGREEN);
+        DrawModelEx(model, grassBlades[i].position, (Vector3){1.0f, 1.0f, 0.0f}, grassBlades[i].rotation*bendfactor, (Vector3){grassBlades[i].scale, grassBlades[i].scale, grassBlades[i].scale}, DARKGREEN);
     }
 }
+
+void DrawGrass(Model model, Material material, Matrix transform)
+{
+    for (int i = 0; i < NUM_GRASS_BLADES; i++)
+    {
+        DrawMesh(model.meshes[0],material,transform);
+    }
+}
+
 
 int main(void)
 {
@@ -75,8 +86,14 @@ int main(void)
 
     int cameraMode = CAMERA_THIRD_PERSON;
 
+    Material material = LoadMaterialDefault();
+    // material.maps[MATERIAL_MAP_DIFFUSE].color = RED;
     // The Model
     Model model = LoadModel("./grass.obj");
+
+        float time = 0.0f;
+        float direction = 1;
+    
 
     DisableCursor();
     SetTargetFPS(60);
@@ -84,15 +101,29 @@ int main(void)
     InitGrass();
     while (!WindowShouldClose())
     {
+        time += GetFrameTime();
+        float bendFactor = sinf(time * 2.0f) * 2.0f*DEG2RAD;
+        // float minAngle=-45.0f*DEG2RAD;
+        // float maxAngle=45.0f*DEG2RAD;
+        // if (bendFactor>maxAngle){
+        //     direction*=-1;
+        // }
+        // else if(bendFactor<minAngle){
+        //     direction*=-1;
+        // }
+        // printf("%f\n",bendFactor);
+        // bendFactor = fmaxf(minAngle, fminf(maxAngle, bendFactor));
+
         UpdateCamera(&camera, cameraMode);
+        // Matrix transforms = MatrixMultiply(MatrixTranslate( 1.5f, 0.0f, 0.0f), MatrixRotateX(bendFactor-0.5f));
 
 
         BeginDrawing();
         ClearBackground(GRAY);
 
         BeginMode3D(camera);
-        DrawPlane((Vector3){0.0f, 0.0f, 0.0f}, (Vector2){32.0f, 32.0f}, LIME);
-        DrawGrass(model);
+        DrawPlane((Vector3){0.0f, 0.0f, 0.0f},(Vector2){32.0f, 32.0f}, LIME);
+        DrawGrassNew(model, bendFactor);
         if (cameraMode == CAMERA_THIRD_PERSON)
         {
             DrawCube(camera.target, 0.5f, 0.5f, 0.5f, PURPLE);
